91f1b6549adf1a54696a774373b69a1a58859127
RudranshGautam <158027721+RudranshGautam@users.noreply.github.com>
2024-08-21 11:44:38 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,40 @@
+const db = require('../util/database');
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    if (this.id) {
+      // Update existing product
+      return db.execute(
+        'UPDATE products SET title = ?, price = ?, description = ?, imageUrl = ? WHERE id = ?',
+        [this.title, this.price, this.description, this.imageUrl, this.id]
+      );
+    } else {
+      // Insert new product
+      return db.execute(
+        'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
+        [this.title, this.price, this.description, this.imageUrl]
+      );
+    }
+  }
+
+  static deleteById(id) {
+    return db.execute('DELETE FROM products WHERE id = ?', [id]);
+  }
+
+  static fetchAll() {
+    return db.execute('SELECT * FROM product');
+  }
+
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE id = ?', [id]);
+  }
+};
+
